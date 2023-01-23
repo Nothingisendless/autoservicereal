ee51fef5a03e47cf6efd90dc285c8064d08700b2
Nothingisendless <liucijus.urmonas@gmail.com>
2023-01-23 13:52:09 +0200

    5-tos paskaitos pirma uzduotis

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import AutomobilioModelis, Automobilis, Paslauga, Uzsakymas
+from .models import Paslauga, Automobilis, Uzsakymas
 
+# Create your views here.
 def index(request):
-    return HttpResponse("Labas pasauli")
+    paslaugu_kiekis = Paslauga.objects.count()
+    automobiliu_kiekis = Automobilis.objects.count()
+    atliktu_uzsakymu_kiekis = Uzsakymas.objects.filter(status__exact='i').count()
+
+    kontekstas = {
+        "paslaugu_kiekis": paslaugu_kiekis,
+        "automobiliu_kiekis": automobiliu_kiekis,
+        "atliktu_uzsakymu_kiekis": atliktu_uzsakymu_kiekis,
+    }
+    return render(request, 'index.html', context=kontekstas)
+
+
+def automobiliai (request):
+    automobiliai = Automobilis.objects.all()
+    context = {
+        'automobiliai' : automobiliai
+    }
+    return render(request, 'automobiliai.html', context=context)
+
+
+def automobilis(request):
